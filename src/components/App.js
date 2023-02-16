d315cf198c03736c73b91a4c94f5c9d70fbb12ab
Shetty02 <shettynehal87@gmail.com>
2023-02-16 19:05:28 +0530

    Done the Task

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  const [name,setName] = useState("");
+    
+    const handleChange = (event) =>{
+        setName(event.target.value);
+    }
   return (
-    <Welcome name="" />
+    <div>
+
+    Name:<input type="text" onChange={handleChange}/> 
+    <Welcome name={name} />
+   </div> 
   )
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React, { useState } from 'react'
+
+function Welcome({name}) {
+  return (
+    <div>
+        <h1> Hey! {name}</h1>;
+        <h2>Welcome to School</h2>
+    </div>
+  )
+}
+
+export default Welcome
